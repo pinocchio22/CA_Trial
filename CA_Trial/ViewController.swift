255172203f777adb075237676becca3e4de63eff
p2glet <vviian.2@gmail.com>
2024-05-03 18:06:40 +0900

    Initial Commit

---
+++ CA_Trial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CA_Trial
+//
+//  Created by JINHUN CHOI on 2024/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
