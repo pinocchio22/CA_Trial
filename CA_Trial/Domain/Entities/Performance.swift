0d35df571ff00b6bd53dcb1e71a31b955106d493
p2glet <vviian.2@gmail.com>
2024-05-13 22:41:38 +0900

    [FEAT] : created Performance

---
+++ CA_Trial/Domain/Entities/Performance.swift
@@ -0,0 +1,23 @@
+//
+//  Performance.swift
+//  CA_Trial
+//
+//  Created by JINHUN CHOI on 2024/05/08.
+//
+
+import Foundation
+
+struct Performance {
+    let codename, guname, title, date: String
+    let place, orgName, useTrgt: String
+    let player: String
+    let program: String
+    let etcDesc: String
+    let orgLink: String
+    let mainImg: String
+    let rgstdate: String
+    let ticket: Ticket
+    let strtdate, endDate: String
+    let themecode: String
+    let isFree: IsFree
+}
